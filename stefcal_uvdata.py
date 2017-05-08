9ef9f6af08f61b31f3cf8a7f3038f8d4bafcee09
Aaron Ewall-Wice <aaronew@mit.edu>
2017-05-08 15:17:42 -0400

    added noise computation methods to stefcal_uvdata.py

--- stefcal_uvdata.py
+++ stefcal_uvdata.py
@@ -1,5 +1,6 @@
 from pyuvdata import UVData
 from pyuvdata import UVCal
+import pyuvdata.parameter as uvp
 from calFlagWeights import CalFlagWeights
 import stefcal
 import uuid
@@ -35,17 +36,35 @@ class StefCalUVData():
     cal_flag_weights: CalFlagWeights object storing flags and weights
     cal_solution: uvcal object
     meta_params: StefCalMeta object containing information on calibration
-                 run
+                 run.
+    _noise_tavg: uv_parameter for noise (uncalib) on each frequency channel. 
+    _noise_favg: uv_parameter for noise (uncalib) at each time
     id: unique uuid corresponding to calibration run. 
     """
+    
     def __init__(self,refant=0,n_phase_iter=5,
                  n_cycles=1,min_bl_per_ant=2,eps=1e-10,
-                 min_ant_times=1,trim_neff=False):
+                 min_ant_times=1,trim_neff=False,spw=0):
         self.id=uuid.uuid4()
         self.model_vis=UVData()
         self.measured_vis=UVData()
         self.cal_flag_weights=CalFlagWeights(self.id)
         self.cal_solution=UVCal()
+        self.spw=spw
+        self._noise_tavg=uvp.UVParameter('noise_tavg',
+                                         description='noise levels in uncalibrated'
+                                         'visibilities computed by taking differences'
+                                         'in frequency and restricted average over'
+                                         ' all times',
+                                         form=('Nbls','NFreqs','Npols'),
+                                         expected_type=np.float)
+        self._noise_favg=uvp.UVParameter('noise_favg',
+                                         description='noise levels in uncalibrated'
+                                         'visibilities computed by taking differences'
+                                         'in time and restricted average over'
+                                         ' all frequency',
+                                         form=('Nblts','Npols'),
+                                         expected_type=np.float)
         self.meta_params=StefCalMeta(refant,
                                      n_phase_iter,
                                      n_cycles,
@@ -136,7 +155,6 @@ class StefCalUVData():
             
         return checkpass
                             
-        
     def _check_consistency(self):
         """
         check consistency between data and model uvdata objects
@@ -161,6 +179,7 @@ class StefCalUVData():
         #need to throw an error here if the model column does not exist
         self.model_vis.read_ms(msname,columnname='MODEL')
         self.measured_vis.read_ms(msname,columnname='DATA')
+        
     def _load_fhd(self,dataname):
         '''
         read in fhd visibility files
@@ -177,6 +196,7 @@ class StefCalUVData():
         '''
         self.model_vis.read_uvfits(modelname)
         self.measured_vis.read_uvfits(dataname)
+        
     def _load_miriad(self,dataname,modelname):
         '''
         read in miriad files
@@ -186,6 +206,56 @@ class StefCalUVData():
         self.model_vis.read_mirad(modelname)
         self.measured_vis.read_miraid(dataname)
         
+    def _compute_noise(self,mode='AUTO'):
+        """
+        compute thermal noise levels for each baseline and frequency
+        using adjacent time differencing and adjacent channel differencing.
+        Args:
+             mode='AUTO','FREQ', or 'TIME' determines over which axis to compute
+             averages. while the other will be diffed if mode='AUTO' is selected, 
+             will perform the averaging over whichever 
+             axis has more samples. 
+        """
+        assert mode in ['AUTO','TIME','FREQ']
+        if self.measured_vis.Nfreqs<=self.model_vis.Ntimes\
+           and mode=='AUTO':
+            mode='FREQ'
+        elif self.measured_vis.nFreqs>=self.model_vis.Ntimes\
+             and mode=='AUTO':
+            mode='TIME'
+        if mode=='FREQ':
+            #take diff in freq and average over freq
+            self.noise_favg=np.zeros((self.measured_vis.Nblts,self.measured_vis.Npols))
+            for pol in range(self.measured_vis.Npols):
+                for blt in range(self.measured_vis.Nblts):
+                    data_select=self.measured_vis.data_array[blt,:,self.spw,pol].squeeze()
+                    flag_select=self.cal_flag_weights.flag_array[blt,:,self.spw,pol].squeeze()
+                    #only take diffs for adjacent channels,
+                    ndiffs=0
+                    for chan in range(1,self.measured_vis.Nfreq):
+                        if not(flag_select[chan]) and not(flag_select[chan-1]):
+                            self.noise_favg[blt,pol]+=np.abs(data_select[chan]-data_select[chan-1])**2.
+                            ndiffs+=1
+                    self.noise_favg[blt,pol]/=2*ndiffs
+        if mode=='TIME':
+            #take diff in time and average over time
+            self.noise_tavg=np.zeros((self.measured_vis.NBls,self.measured_vis.NFreqs,self.Npols))
+            for pol in range(self.measured_vis.Npols):
+                for blnum in self.measured_vis.Nbls
+                    bl_selection=self.baseline_array==self.baseline_array[blnum]
+                    ndiffs=0
+                    for chan in range(self.vis_measured.NFreqs):
+                        data_select=self.measured_vis.data_array[bl_selection,self.spw,chan,pol]
+                        flag_select=self.cal_flag_weights.flag_array[bl_selection,self.spw,chan,pol]
+                        #only take diffs for adjacent channels,
+                        ndiffs=0
+                        for tnum in range(1,self.measured.NTimes):
+                            if not(flag_select[tnum]) and not(flag_select[tnum-1]):
+                                self.noise_tavg[blnum,chan,pol]+=\
+                                np.abs(data_select[tnum]-data_select[tnum-1])**2.
+                                ndiffs+=1
+                        self.noise_tavg[blnum,chan,pol]/=(2.*ndiffs)
+                                
     def _read_files(self,data,mode,flagweights_fromdata,flagweightsfile=None,model=None):
         '''
         read in all files including data,model,calibration weights
@@ -196,18 +266,18 @@ class StefCalUVData():
         '''
         assert mode in ['MS','UVFITS','MIRIAD','FHD']
         if mode=='MS':
-            self.__load_ms(data)
+            self._load_ms(data)
             self.meta_params.model_file=data
         elif mode=='UVFITS':
             assert model
-            self.__load_uvfits(data,model)
+            self._load_uvfits(data,model)
             self.meta_params.model_file=model
         elif mode=='MIRIAD':
             assert model
             self.__load_miriad(data,model)
             self.meta_params.model_file=model
         elif mode=='FHD':
-            self.__load_fhd(data)
+            self._load_fhd(data)
         if(flagweights_fromdata):
             self.cal_flag_weights.from_data(self.model_data)
         else:
@@ -215,6 +285,7 @@ class StefCalUVData():
             self.cal_flag_weights.from_file(flagweightsfile)
         self.meta_params.data_file=data
         self._check_consistency()
+        self._compute_noise()
     def from_ms(self,msfile,flagweights_fromdata,flagweightsfile=None):
         """
         initialize stefcal from a measurement set
@@ -268,7 +339,6 @@ class StefCalUVData():
                          flagweights_fromdata=flagweights_fromdata,
                          flagweightsfile=flagweightsfile,
                          model=uvfitsmodel)
-        
     
     def stefcalibrate(self,parallelized=False):
         '''
