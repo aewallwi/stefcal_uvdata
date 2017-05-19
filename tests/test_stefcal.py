1e8721c9516cbb5a632d6dd9a10c8200295e83cd
Aaron Ewall-Wice <aaronew@mit.edu>
2017-05-19 10:28:28 -0400

    added test of matrix and baseline-time reordering

--- tests/test_stefcal.py
+++ tests/test_stefcal.py
@@ -1,5 +1,6 @@
 """Tests for stefcal uvdata"""
 import nose.tools as nt
+import numpy as np
 import os
 from stefcal_uvdata import StefcalUVData
 from data import DATA_PATH
@@ -31,7 +32,20 @@ def test_read_write():
     stefcal_perfect.load_state(DATA_PATH+'/test/perfect_cal')
     print('reading succesful')
 
+def test_matrix_reordering():
+    """
+    Test whether method that converts from matrix to baseline-times 
+    reverses correctly
+    """
+    stefcal_test=StefcalUVData()
+    measured_vis=os.path.join(DATA_PATH,'point_source_sim_model.uvfits')
+    model_vis=os.path.join(DATA_PATH,'point_source_sim_model.uvfits')
+    stefcal_test.from_uvfits(measured_vis,model_vis,
+                             flag_weights_fromdata=True)
+    time_array_test=stefcal_test._matrix_2_blt_list(stefcal_test._blt_list_2_matrix(stefcal_test.measured_vis.time_array,range(stefcal_test.measured_vis.Ntimes)))
+    nt.assert_true(np.all(time_array_test==stefcal_test.measured_vis.time_array))
 
+    
 def test_perfect_calibration():
     """
     Test stefcal on noiseless data set with unity gains
@@ -59,7 +73,7 @@ def test_perfect_calibration():
 
     #Chi square should be infinite
     
-
-test_perfect_calibration()
+test_matrix_reordering()
+#test_perfect_calibration()
     
     
